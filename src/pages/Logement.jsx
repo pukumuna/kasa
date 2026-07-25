6f21324a53620c4f761d5e352b58394b8791b794
Georges KABUKU <zolhom13@gmail.com>
2026-07-25 14:50:29 +0200

    Ajout du composant Carousel et son Utilisation dans la page Logement.jsx. Affichage des pictures et propriétés de Logement

--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -1,15 +1,49 @@
-import { Link, useParams } from 'react-router-dom'
+import { Navigate, useParams } from 'react-router-dom'
+import Carousel from '../components/Carousel.jsx'
+import Rating from '../components/Rating.jsx'
+import { locationsData } from '../data/locations.js'
 
-export default function Logement() {
+const Logement = () => {
   const { id } = useParams()
+  const location = locationsData.find((item) => item.id === id)
+
+  if (!location) {
+    return <Navigate to="/404" replace />
+  }
+
+  const hostParts = location.host.name.split(' ')
+  const hostFirstName = hostParts.shift()
+  const hostLastName = hostParts.join(' ')
 
   return (
-    <section>
-      <h1>Détail du logement</h1>
+    <article className="housing">
+      <Carousel pictures={location.pictures} title={location.title} />
 
-      <p>Identifiant du logement : {id}</p>
+      <div className="housing__summary">
+        <div className="housing__identity">
+          <h1>{location.title}</h1>
+          <p>{location.location}</p>
 
-      <Link to="/">Retour à l'accueil</Link>
-    </section>
+          <ul className="tags" aria-label="Caractéristiques">
+            {location.tags.map((tag) => (
+              <li key={tag}>{tag}</li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="housing__host-rating">
+          <div className="host">
+            <p>
+              <span>{hostFirstName}</span>
+              <span>{hostLastName}</span>
+            </p>
+            <img src={location.host.picture} alt={`Portrait de ${location.host.name}`} />
+          </div>
+          <Rating value={location.rating} />
+        </div>
+      </div>
+      
+    </article>
   )
 }
+export default Logement
